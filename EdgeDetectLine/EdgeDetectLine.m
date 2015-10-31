e52834325b49adc00668c47c1a85052b9a56fd56
sx4452 <zb.benzhang@gmail.com>
2015-10-31 23:02:23 +0800

    201510312302

--- EdgeDetectLine/EdgeDetectLine.m
+++ EdgeDetectLine/EdgeDetectLine.m
@@ -1,22 +1,21 @@
 
 function EdgeDetectLine
-clc
-close
 I = imread('circuit.png');
 I = rgb2gray(I);
 I = im2double(I);
-I_edge = edge(I, 'canny');
-%I_edge = im2double(I_edge);
-imwrite(I_edge, 'edgeout.jpg');
+%imwrite(I, 'circuitgray.jpg');
+I_edge = edge(I);
+k=5;
+%imwrite(I_edge, 'edgeout.jpg');
 [H, theta, rho] = hough(I_edge);
-peaks = houghpeaks(H,5);
-I_edge = im2double(I_edge);
-lines = houghlines(I_edge, theta, rho, peaks, 'FillGap', 5);
+%nhoodsize = uint8(0.1*size(H)/50);
+peaks = houghpeaks(H, k);
+lines = houghlines(I_edge, theta, rho, peaks, 'FillGap', 100);
 figure
 imshow(I_edge)
 hold on    
 for k=1:length(lines)    
     xy=[lines(k).point1;lines(k).point2];    
-    plot(xy(:,1),xy(:,2),'LineWidth',4,'Color','red');    
+    plot(xy(:,1),xy(:,2),'LineWidth',1,'Color','red');    
 end  
 end
